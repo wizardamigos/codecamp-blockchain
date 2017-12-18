0396661f039f54666e35111f678c1334202fadb7
Alexander Praetorius <dev@serapath.de>
2017-12-18 23:45:33 +0700

    add first draft

---
+++ index.js
@@ -0,0 +1,11 @@
+
+var dc = require('dom-console')()
+dc.api.toggle()
+
+var minixhr = require('minixhr')
+var csjs = require('csjs-inject')
+var bel = require('bel')
+
+console.info('Hello CodeCamper')
+console.log('Learn yourself something about')
+console.error('BLOCKCHAINS')
